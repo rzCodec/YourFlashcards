5e9088e3ac450e360f8b9825b2a8b5a81131cb7b
Ronald <laironald52@gmail.com>
2021-11-21 11:47:15 +0200

    Update AppDatabase.java

--- app/src/main/java/myapp/your_flashcards/Room_Database/AppDatabase.java
+++ app/src/main/java/myapp/your_flashcards/Room_Database/AppDatabase.java
@@ -8,10 +8,6 @@ import android.content.Context;
 import myapp.your_flashcards.Flashcard.Flashcard;
 import myapp.your_flashcards.Subject.Subject;
 
-/**
- * Created by User on 2018/06/12.
- */
-
 @Database(entities = {Subject.class, Flashcard.class}, version = 3, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
@@ -23,9 +19,7 @@ public abstract class AppDatabase extends RoomDatabase {
     public static AppDatabase getAppDatabase(Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    AppDatabase.class, "subject-database")
-                            // allow queries on the main thread.
-                            // Don't do this on a real app! See PersistenceBasicSample for an example.
+                    AppDatabase.class, "subject-database")                   
                             .fallbackToDestructiveMigration()
                             .build();
         }
