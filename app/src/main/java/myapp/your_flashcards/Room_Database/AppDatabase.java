5a010fa3b8a6547802a747a6234723d8c9cf3d5b
Ronald <laironald52@gmail.com>
2021-11-21 11:52:56 +0200

    Update AppDatabase.java

--- app/src/main/java/myapp/your_flashcards/Room_Database/AppDatabase.java
+++ app/src/main/java/myapp/your_flashcards/Room_Database/AppDatabase.java
@@ -4,15 +4,12 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import android.content.Context;
-
 import myapp.your_flashcards.Flashcard.Flashcard;
 import myapp.your_flashcards.Subject.Subject;
 
 @Database(entities = {Subject.class, Flashcard.class}, version = 3, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
-
     private static AppDatabase INSTANCE;
-
     public abstract Subject_Dao subjectDao();
     public abstract Flashcard_Dao flashcard_Dao();
 
